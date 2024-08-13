95e13b09c6235d0f3d41effefeb8a474b01eb4a9
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-13 14:29:16 -0500

    Project creation and connection to Amazon Web service

---
+++ src/main/java/com/example/demospringboot3/Demospringboot3Application.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospringboot3Application.class, args);
+	}
+
+}
